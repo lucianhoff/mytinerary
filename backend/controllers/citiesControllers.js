e1c27c2b6c1e9cad34ae3e284d26a2ad99937c6c
lucianhoff <lucianoaguirre007@gmail.com>
2021-11-27 04:03:49 -0300

    casi mvp

--- backend/controllers/citiesControllers.js
+++ backend/controllers/citiesControllers.js
@@ -5,7 +5,7 @@ const cities = [
     description:
       "Tokyo, Japan’s busy capital, mixes the ultramodern and the traditional, from neon-lit skyscrapers to historic temples.",
     image: "Tokyo",
-    id: 0,
+    divisa: ""
   },
   {
     countryName: "Japan",
@@ -13,7 +13,6 @@ const cities = [
     description:
       "Osaka is a large port city and commercial center on the Japanese island of Honshu. It's known for its modern architecture, nightlife and hearty street food.",
     image: "Osaka",
-    id: 1,
   },
   {
     countryName: "China",
@@ -21,7 +20,6 @@ const cities = [
     description:
       "Shanghai, on China’s central coast, is the country's biggest city and a global financial hub.",
     image: "Shanghai",
-    id: 2,
   },
   {
     countryName: "Thailand",
@@ -29,7 +27,6 @@ const cities = [
     description:
       "Bangkok, Thailand’s capital, is a large city known for ornate shrines and vibrant street life",
     image: "Bangkok",
-    id: 3,
   },
   {
     countryName: "Germany",
@@ -37,7 +34,6 @@ const cities = [
     description:
       "Berlin, Germany’s capital, dates to the 13th century. Reminders of the city's turbulent 20th-century history include its Holocaust memorial and the Berlin Wall's graffitied remains.",
     image: "Berlin",
-    id: 4,
   },
   {
     countryName: "Russia",
@@ -45,7 +41,6 @@ const cities = [
     description:
       "Moscow, on the Moskva River in western Russia, is the nation’s cosmopolitan capital. In its historic core is the Kremlin, a complex that’s home to the president and tsarist treasures in the Armoury.",
     image: "Moscow",
-    id: 5,
   },
   {
     countryName: "UK",
@@ -53,7 +48,6 @@ const cities = [
     description:
       "London, the capital of England and the United Kingdom, is a 21st-century city with history stretching back to Roman times. At its centre stand the imposing Houses of Parliament, the iconic ‘Big Ben’ clock tower and Westminster Abbey, site of British monarch coronations. ",
     image: "London",
-    id: 6,
   },
   {
     countryName: "France",
@@ -61,7 +55,6 @@ const cities = [
     description:
       "Paris, France's capital, is a major European city and a global center for art, fashion, gastronomy and culture. Its 19th-century cityscape is crisscrossed by wide boulevards and the River Seine.",
     image: "Paris",
-    id: 7,
   },
   {
     countryName: "Mexico",
@@ -69,7 +62,6 @@ const cities = [
     description:
       "Guadalajara is a city in western Mexico. It’s known for tequila and mariachi music, both born in Jalisco, the state of which Guadalajara is the capital.",
     image: "Guadalajara",
-    id: 8,
   },
   {
     countryName: "Argentina",
@@ -77,7 +69,6 @@ const cities = [
     description:
       "Buenos Aires is Argentina’s big, cosmopolitan capital city. Its center is the Plaza de Mayo, lined with stately 19th-century buildings including Casa Rosada, the iconic, balconied presidential palace.",
     image: "BuenosAires",
-    id: 9,
   },
   {
     countryName: "Brazil",
@@ -85,7 +76,6 @@ const cities = [
     description:
       "Rio de Janeiro is a huge seaside city in Brazil, famed for its Copacabana and Ipanema beaches, 38m Christ the Redeemer statue atop Mount Corcovado and for Sugarloaf Mountain, a granite peak with cable cars to its summit.",
     image: "RioDeJaneiro",
-    id: 10,
   },
   {
     countryName: "Peru",
@@ -93,7 +83,6 @@ const cities = [
     description:
       "Lima, the capital of Peru, lies on the country's arid Pacific coast. Though its colonial center is preserved, it's a bustling metropolis and one of South America’s largest cities.",
     image: "Lima",
-    id: 11,
   },
   {
     countryName: "Uruguay",
@@ -101,7 +90,6 @@ const cities = [
     description:
       "Punta del Este is a city and seaside resort on the Atlantic Coast in the Maldonado Department of southeastern Uruguay.",
     image: "PuntaDelEste",
-    id: 12,
   },
   {
     countryName: "Greece",
@@ -109,7 +97,6 @@ const cities = [
     description:
       "Athens is the capital of Greece. It was also at the heart of Ancient Greece, a powerful civilization and empire.",
     image: "Athens",
-    id: 13,
   },
   {
     countryName: "United Arab Emirates",
@@ -117,22 +104,75 @@ const cities = [
     description:
       "Dubai is a city and emirate in the United Arab Emirates known for luxury shopping, ultramodern architecture and a lively nightlife scene.",
     image: "Dubai",
-    id: 14,
   },
 ];
 
-const citiesControllers = {
-  getAllCities: (req, res) => {
-    res.json({ response: cities });
-  },
+const City = require('../models/City')
 
-  loadCity: (req, res) => {
-    res.json({ response: cities });
+const citiesControllers = {
+  getAllCities: async(req, res) => {
+    let cities
+    let error = null
+
+    try { 
+      cities = await City.find()
+    }catch(err) {
+      error = err 
+      console.log(error)
+    }
+
+    res.json({
+      resonse: error ? 'ERROR' : cities, 
+      success: error ? false : true,
+      error: error
+    })
+
   },
-  getCity: (req, res) => {
-    const id = req.params.id;
-    const city = cities.find((city) => city.id.toString() === id);
-    res.json({ response: { city } });
+  addCity: (req, res) => {
+    const {cityName, countryName, description, image } = req.body
+    new City({cityName, countryName, description, image }).save()
+    .then((response) => res.json({response}))
   },
+  getCity: async(req, res) => {
+
+    let cities
+    const id = req.params.id
+
+    try {
+      cities = await City.findOne({_id:id})
+    } catch(err) {
+      console.log(err)
+    }
+
+    res.json({response: cities, success: true})
+    
+  },
+  deteleCity: async(req, res) => {
+
+    const id = req.params.id
+    let cities
+    try {
+      await City.findOneAndDelete({_id:id})
+      cities = await City.find()
+    } catch(err) {
+      console.log(err)
+    }
+
+    res.json({response: cities, success: true})
+  },
+  modifyCity: async(req, res) => {
+    let id = req.params.id
+    let cities = req.body
+    let update
+    console.log(cities);
+    try { 
+      update = await City.findOneAndUpdate({_id:id}, cities, {new:true})
+      console.log(update)
+    } catch(err) {
+      console.log(err)
+    }
+    res.json({success:update ? true : false})
+  }
+
 };
 module.exports = citiesControllers;
--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -2,12 +2,15 @@ const Router = require('express').Router();
 
 const citiesControllers = require('../controllers/citiesControllers')
 
-const {getAllCities, getCity, loadCity} = citiesControllers;
+const {getAllCities, getCity, addCity, deteleCity, modifyCity} = citiesControllers;
 
 Router.route('/cities')
  .get(getAllCities)
+ .post(addCity)
 
-Router.route('/cities/:id')
+ Router.route('/cities/:id')
  .get(getCity)
+ .delete(deteleCity)
+ .put(modifyCity)
 
  module.exports = Router
